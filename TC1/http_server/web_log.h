66bd1bca0dad63bd4667d9944d1b8481f23a4a42
zogodo <742782908@qq.com>
2022-04-06 00:52:58 +0800

    use old web_log()

--- TC1/http_server/web_log.h
+++ TC1/http_server/web_log.h
@@ -17,6 +17,19 @@ void SetLogRecord(LogRecord* lr, char* log);
 char* GetLogRecord();
 void WebLog(const char *M, ...);
 
-#define web_log(N, M, ...) WebLog("["N" %s:%d] "M, SHORT_FILE, __LINE__, ##__VA_ARGS__)
+#define TIM_LEN 32
+extern LogRecord log_record;
+extern char* LOG_TMP;
+extern time_t now;
+extern char time_buf[];
+#define web_log(N, M, ...)                           \
+    LOG_TMP = (char*)malloc(sizeof(char)*LOG_LEN);     \
+    now = time(NULL); \
+    now += 28800; \
+    strftime(time_buf, TIM_LEN, "%Y-%m-%d %H:%M:%S", localtime(&now)); \
+    snprintf(LOG_TMP, LOG_LEN, "[%s][%s %s:%d] "M, time_buf, N, SHORT_FILE, __LINE__, ##__VA_ARGS__); \
+    SetLogRecord(&log_record, LOG_TMP);                \
+
+#define web_log0(N, M, ...) WebLog("["N" %s:%d] "M, SHORT_FILE, __LINE__, ##__VA_ARGS__)
 
 #endif // !WEB_LOG_H
