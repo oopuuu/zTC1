56395f62bc3e53aac73b26f1e612e7bbe6f830c4
吴永福 <qwuyongfuq@live.cn>
2025-03-19 17:20:45 +0800

    修改生成ota年月日时分秒.bin和all年月日时分秒.bin

--- build.sh
+++ build.sh
@@ -8,4 +8,7 @@ mico make clean
 
 mico make TC1@MK3031@moc total
 
-cp ./build/TC1\@MK3031\@moc/binary/TC1\@MK3031\@moc.ota.bin  ./build/TC1\@MK3031\@moc/binary/ota.bin
+cp ./build/TC1\@MK3031\@moc/binary/TC1\@MK3031\@moc.ota.bin ./build/TC1\@MK3031\@moc/binary/ota$(date +%Y%m%d%H%M%S).bin
+
+cp ./build/TC1\@MK3031\@moc/binary/TC1\@MK3031\@moc.all.bin ./build/TC1\@MK3031\@moc/binary/all$(date +%Y%m%d%H%M%S).bin
+
